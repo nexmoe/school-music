f71aa1ee7c1a5141792749c24442f31a353fe431
Nexmoe <nexmoe@qq.com>
2022-08-17 11:48:35 +0800

    unplugin-vue-components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
-import { defineNuxtConfig } from 'nuxt'
-
+import { defineNuxtConfig } from "nuxt";
+const { VantResolver } = require("unplugin-vue-components/resolvers");
+const ComponentsPlugin = require("unplugin-vue-components/webpack");
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+	webpack: {
+		plugins: [
+			ComponentsPlugin({
+				resolvers: [VantResolver()],
+			}),
+		],
+	},
+});
