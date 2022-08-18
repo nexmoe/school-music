9de3afb83e2e347b507325bd599adfdb16dcf3bd
Nexmoe <nexmoe@qq.com>
2022-08-18 22:26:48 +0800

    chore: pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ const { VantResolver } = require("unplugin-vue-components/resolvers");
 const ComponentsPlugin = require("unplugin-vue-components/webpack");
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	buildModules: ["@pinia/nuxt"],
 	webpack: {
 		plugins: [
 			ComponentsPlugin({
